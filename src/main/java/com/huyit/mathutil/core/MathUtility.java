63c0afbbcf52d843a51655b46bd0da2f3e147620
server-craftsman <huyit2003@gmail.com>
2024-06-05 19:38:47 +0700

    Update main code to see red alert

--- src/main/java/com/huyit/mathutil/core/MathUtility.java
+++ src/main/java/com/huyit/mathutil/core/MathUtility.java
@@ -20,7 +20,7 @@ public class MathUtility {
     //0! = 1
     public static long getFactorial(int n) {
 
-        long product = 1;//biến con heo đất, biến nhân dồn
+        long product = 10;//biến con heo đất, biến nhân dồn
         
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0...20");
